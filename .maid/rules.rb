6fcd9bc91b6229a7a8364e6488d5290a34ba0bec
Brad Parbs <brad@bradparbs.com>
2014-11-16 13:33:51 -0600

    more maid rules

--- .maid/rules.rb
+++ .maid/rules.rb
@@ -18,4 +18,32 @@ Maid.rules do
 		end
 	end
 
+	#
+	# Update all our Applications, brew, oh my zsh and such
+	#
+	rule 'Updating Brew' do
+		`brew update`
+	end
+	rule 'Updating Cask Appliactions' do
+		`brew cask update`
+	end
+	rule 'Updating Oh My Zsh' do #This is a kind of hacky way to do it
+		`cd ~/.oh-my-zsh && g add . && git commit -m "local updates" && git pull --rebase origin master`
+	end
+
+	#
+	# Downloads Folder Stuff
+	#
+	rule 'Trash incomplete downloads' do
+		trash dir('~/Downloads/*.download').select { |p| 3.days.since modified_at p }
+	end
+
+	rule 'Trash zips and tarballs downloaded from GitHub' do
+		dir('~/Downloads/*.{zip,tgz,gz,rar,tar}').each do |path|
+			if downloaded_from(path).any? { |u| u.match %r{//([^\/]+\.)?github\.com\/} }
+				trash path
+			end
+		end
+	end
+
 end
