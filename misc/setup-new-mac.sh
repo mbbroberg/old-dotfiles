ff2385473f3e2fc38e417d130e52388636f5d4bd
Brad Parbs <brad@bradparbs.com>
2014-12-08 14:25:31 -0600

    Fix setup-new-mac.sh to follow our newly tracked files

--- misc/setup-new-mac.sh
+++ misc/setup-new-mac.sh
@@ -24,8 +24,8 @@ brew install git
 
 echo "Git config"
 
-git config --global user.name "Brad Parbs"
-git config --global user.email brad@bradparbs.com
+#git config --global user.name "Brad Parbs"
+#git config --global user.email brad@bradparbs.com
 
 
 echo "Installing brew git utilities..."
@@ -54,24 +54,16 @@ brew install caskroom/cask/brew-cask
 
 echo "Copying dotfiles from Github"
 cd ~
-git clone git@github.com:bradp/dotfiles.git dotfiles
+git clone --recursive git@github.com:bradp/dotfiles.git dotfiles
 cd ~/dotfiles
-sh symlink_dot_files
+rake
 
 echo "Grunting it up"
 npm install -g grunt-cli
 
 #Install Zsh & Oh My Zsh
 echo "Installing Oh My ZSH..."
-curl -L http://install.ohmyz.sh | sh
-
-echo "Setting up Oh My Zsh theme..."
-cd  /Users/bradparbs/.oh-my-zsh/themes
-curl https://gist.githubusercontent.com/bradp/a52fffd9cad1cd51edb7/raw/cb46de8e4c77beb7fad38c81dbddf531d9875c78/brad-muse.zsh-theme > brad-muse.zsh-theme
-
-echo "Setting up Zsh plugins..."
-cd ~/.oh-my-zsh/custom/plugins
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+./~/dotfiles/.oh-my-zsh/install.sh
 
 echo "Setting ZSH as shell..."
 chsh -s /bin/zsh
