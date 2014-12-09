4dc9b41b4a62c439c1d66c9c0e565244e2b6f7ad
Brad Parbs <brad@bradparbs.com>
2014-12-09 09:41:26 -0600

    Add dependency installation to bootstrap.sh

--- misc/boostrap.sh
+++ misc/boostrap.sh
@@ -1,17 +1,19 @@
-#!/bin/bash
-#
+#!/bin/sh
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+	if test ! $(which brew); then
+	  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+	brew install git git-extras legit git-flow ctags tig tree wget trash svn imagemagick cowsay fortune
+	brew cleanup
+	sh ~/dotfiles/.oh-my-zsh/tools/install.sh
+	chsh -s $(which zsh)
+fi
+
+if [[ "$OSTYPE" =~ ^linux ]]; then
+	sudo apt-get -y install zsh tree tig wget cowsay fortune rake
+	chsh -s $(which zsh)
+fi
 
 git clone --recursive https://github.com/bradp/dotfiles.git ~/dotfiles
-
-if test ! $(which rake); then
-	if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-		sudo apt-get -y rake
-	elif [ "$(uname)" == "Darwin" ]; then
-		if test ! $(which brew); then
-			ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		fi
-		brew install rake
-	fi
-fi
 
 cd ~/dotfiles && rake
