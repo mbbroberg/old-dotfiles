b3b6ec938d98a211d1cf53211165795a57cb9a94
Brad Parbs <brad@bradparbs.com>
2014-12-10 20:19:55 -0600

    bootstrap

--- misc/bootstrap-linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git clone --recursive https://github.com/bradp/dotfiles.git ~/dotfiles
-
-apt-get -y install zsh tree tig wget cowsay fortune rake
-sh ~/dotfiles/.oh-my-zsh/tools/install.sh
-chsh -s $(which zsh)
-
-cd ~/dotfiles && rake
